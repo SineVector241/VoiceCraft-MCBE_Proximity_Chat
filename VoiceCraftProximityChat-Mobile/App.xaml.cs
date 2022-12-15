8647f77663cfc3df021b4e731b2cabbbe60189e7
SineVector241 <flyingbirdasuka1@gmail.com>
2022-12-15 15:09:36 +1100

    Starting on mobile version

---
+++ VoiceCraftProximityChat-Mobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace VoiceCraftProximityChat_Mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ VoiceCraftProximityChat-Mobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace VoiceCraftProximityChat_Mobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
