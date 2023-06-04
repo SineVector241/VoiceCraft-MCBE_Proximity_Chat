fceca8ec8b52a20932988282be008402fe6d8eba
SineVector241 <flyingbirdasuka1@gmail.com>
2023-06-04 12:40:49 +1000

    Started on MultiPlatform migration to IOS. PC moving to xamarin if successful.

---
+++ VoiceCraft/VoiceCraft/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace VoiceCraft
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            testImg.Source = ImageSource.FromResource("VoiceCraft.Resources.Images.bgdark.png");
+        }
+    }
+}
