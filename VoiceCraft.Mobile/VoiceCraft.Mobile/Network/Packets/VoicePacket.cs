6b5819d5c9aadea7d630e3af3766b7fc5bbc4176
SineVector241 <flyingbirdasuka1@gmail.com>
2023-06-09 22:06:10 +1000

    Working on VoicePacket.

--- VoiceCraft.Mobile/VoiceCraft.Mobile/Network/Packets/VoicePacket.cs
+++ VoiceCraft.Mobile/VoiceCraft.Mobile/Network/Packets/VoicePacket.cs
@@ -1,6 +1,26 @@
-﻿namespace VoiceCraft.Mobile.Network.Packets
+﻿using System.Numerics;
+
+namespace VoiceCraft.Mobile.Network.Packets
 {
+    public enum VoicePacketIdentifier
+    {
+        Login,
+        Logout,
+        Accept,
+        Deny,
+        Audio,
+        UpdatePosition,
+        Error,
+        Null
+    }
+
     public class VoicePacket
     {
+        private VoicePacketIdentifier Identifier; //Data containing the packet identifier.
+        private uint Count; //Data containing packet count to detect packet loss.
+        private ushort Key; //Data containing the key of a participant.
+        private Vector3 Position; //Data containing audio source assuming client audio handling is at 0,0,0 rotation 0.
+        private byte[] Audio; //Data containing encoded audio data.
+        private string Version; //Data containing packet version.
     }
 }
