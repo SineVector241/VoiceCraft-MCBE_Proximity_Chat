e08556db6e605db8fce1e27a3d0d6f23c4f87ed7
SineVector241 <flyingbirdasuka1@gmail.com>
2023-06-19 21:37:56 +1000

    Proximity toggle now works properly. You do not need to be in the same dimension.

--- VoiceCraft.Server/Sockets/VoiceLogic.cs
+++ VoiceCraft.Server/Sockets/VoiceLogic.cs
@@ -29,32 +29,43 @@ namespace VoiceCraft.Server.Sockets
             var audioparticipant = ServerData.GetParticipantByVoice(EP);
             if (audioparticipant.Value != null && audioparticipant.Value.Binded && !audioparticipant.Value.Muted)
             {
-                var list = ServerData.Participants.Where(x => x.Value != null && x.Value.Binded && x.Key != audioparticipant.Key && x.Value.MinecraftData.DimensionId == audioparticipant.Value.MinecraftData.DimensionId && Vector3.Distance(x.Value.MinecraftData.Position, audioparticipant.Value.MinecraftData.Position) <= ServerProperties.Properties.ProximityDistance);
-                for (int i = 0; i < list.Count(); i++)
+                if (ServerProperties.Properties.ProximityToggle)
                 {
-                    var participant = list.ElementAt(i);
-                    if (ServerProperties.Properties.ProximityToggle && participant.Value?.SocketData.VoiceAddress != null)
+                    var list = ServerData.Participants.Where(x => x.Value != null && x.Value.Binded && x.Key != audioparticipant.Key && x.Value.MinecraftData.DimensionId == audioparticipant.Value.MinecraftData.DimensionId && Vector3.Distance(x.Value.MinecraftData.Position, audioparticipant.Value.MinecraftData.Position) <= ServerProperties.Properties.ProximityDistance);
+                    for (int i = 0; i < list.Count(); i++)
                     {
-                        var LocalPlayerCoordinates = participant.Value.MinecraftData.Position;
-                        var AudioSourceCoordinates = audioparticipant.Value.MinecraftData.Position;
-                        var LocalPlayerRotation = participant.Value.MinecraftData.Rotation;
-                        var distance = Vector3.Subtract(LocalPlayerCoordinates, AudioSourceCoordinates);
-                        var rotationToSource = Math.Atan2(0 - AudioSourceCoordinates.X, 0 - AudioSourceCoordinates.Z) - (LocalPlayerRotation * (Math.PI / 180));
-                        var cosTheta = Math.Cos(rotationToSource);
-                        var sinTheta = Math.Sin(rotationToSource);
-                        var vec = new Vector3((float)(cosTheta * distance.X - sinTheta * distance.Z), AudioSourceCoordinates.Y - LocalPlayerCoordinates.Y, (float)(sinTheta * distance.X - cosTheta * distance.Z));
+                        var participant = list.ElementAt(i);
+                        if (participant.Value?.SocketData.VoiceAddress != null)
+                        {
+                            var LocalPlayerCoordinates = participant.Value.MinecraftData.Position;
+                            var AudioSourceCoordinates = audioparticipant.Value.MinecraftData.Position;
+                            var LocalPlayerRotation = participant.Value.MinecraftData.Rotation;
+                            var distance = Vector3.Subtract(LocalPlayerCoordinates, AudioSourceCoordinates);
+                            var rotationToSource = Math.Atan2(0 - AudioSourceCoordinates.X, 0 - AudioSourceCoordinates.Z) - (LocalPlayerRotation * (Math.PI / 180));
+                            var cosTheta = Math.Cos(rotationToSource);
+                            var sinTheta = Math.Sin(rotationToSource);
+                            var vec = new Vector3((float)(cosTheta * distance.X - sinTheta * distance.Z), AudioSourceCoordinates.Y - LocalPlayerCoordinates.Y, (float)(sinTheta * distance.X - cosTheta * distance.Z));
 
-                        Packet.PacketPosition = vec;
-                        Packet.PacketKey = audioparticipant.Key;
-                        Packet.PacketDistance = (ushort)ServerProperties.Properties.ProximityDistance;
-                        SendPacket(Packet, participant.Value.SocketData.VoiceAddress);
+                            Packet.PacketPosition = vec;
+                            Packet.PacketKey = audioparticipant.Key;
+                            Packet.PacketDistance = (ushort)ServerProperties.Properties.ProximityDistance;
+                            SendPacket(Packet, participant.Value.SocketData.VoiceAddress);
+                        }
                     }
-                    else if (participant.Value?.SocketData.VoiceAddress != null)
+                }
+                else
+                {
+                    var list = ServerData.Participants.Where(x => x.Value != null && x.Value.Binded && x.Key != audioparticipant.Key);
+                    for (int i = 0; i < list.Count(); i++)
                     {
-                        Packet.PacketPosition = new Vector3();
-                        Packet.PacketKey = audioparticipant.Key;
-                        Packet.PacketDistance = (ushort)ServerProperties.Properties.ProximityDistance;
-                        SendPacket(Packet, participant.Value.SocketData.VoiceAddress);
+                        var participant = list.ElementAt(i);
+                        if (participant.Value?.SocketData.VoiceAddress != null)
+                        {
+                            Packet.PacketPosition = new Vector3();
+                            Packet.PacketKey = audioparticipant.Key;
+                            Packet.PacketDistance = (ushort)ServerProperties.Properties.ProximityDistance;
+                            SendPacket(Packet, participant.Value.SocketData.VoiceAddress);
+                        }
                     }
                 }
             }
