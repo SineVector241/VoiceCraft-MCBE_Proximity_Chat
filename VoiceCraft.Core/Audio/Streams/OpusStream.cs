33e997adb2e7bb53c7e0a48346b4e2d7c5ffd3f2
SineVector241 <flyingbirdasuka1@gmail.com>
2024-02-17 15:14:46 +1100

    New Streams.

---
+++ VoiceCraft.Core/Audio/Streams/OpusStream.cs
@@ -0,0 +1,44 @@
+﻿using Concentus.Structs;
+using NAudio.Wave;
+using System;
+
+namespace VoiceCraft.Core.Audio.Streams
+{
+    public class OpusStream : IWaveProvider
+    {
+        public WaveFormat WaveFormat { get; set; }
+        private readonly OpusDecoder Decoder;
+        private readonly short[] DecodeBuffer;
+        private bool NextMissed;
+
+        public OpusStream(WaveFormat WaveFormat)
+        {
+            this.WaveFormat = WaveFormat;
+            DecodeBuffer = new short[WaveFormat.ConvertLatencyToByteSize(40) / 2];
+            Decoder = new OpusDecoder(WaveFormat.SampleRate, 1);
+        }
+
+        public int Read(byte[] buffer, int offset, int count)
+        {
+            count = !NextMissed || count > 0
+                ? Decoder.Decode(buffer, offset, count, DecodeBuffer, 0, DecodeBuffer.Length, false)
+                : Decoder.Decode(null, 0, 0, DecodeBuffer, 0, DecodeBuffer.Length, false);
+
+            //Convert and put into the buffer.
+            Buffer.BlockCopy(ShortsToBytes(DecodeBuffer, 0, count), 0, buffer, 0, count);
+            return count;
+        }
+
+        private static byte[] ShortsToBytes(short[] input, int offset, int length)
+        {
+            byte[] processedValues = new byte[length * 2];
+            for (int c = 0; c < length; c++)
+            {
+                processedValues[c * 2] = (byte)(input[c + offset] & 0xFF);
+                processedValues[c * 2 + 1] = (byte)(input[c + offset] >> 8 & 0xFF);
+            }
+
+            return processedValues;
+        }
+    }
+}
---
+++ VoiceCraft.Core/Audio/Streams/VoiceCraftStream.cs
@@ -0,0 +1,52 @@
+﻿using NAudio.Wave;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace VoiceCraft.Core.Audio.Streams
+{
+    public class VoiceCraftStream : IWaveProvider, IDisposable
+    {
+        public WaveFormat WaveFormat { get; set; }
+        private BufferedWaveProvider DecodedAudio { get; set; }
+        private OpusStream OpusStream { get; set; }
+        private Task DecodeThread { get; set; }
+        private readonly CancellationTokenSource TokenSource;
+        private readonly CancellationToken Token;
+
+        public VoiceCraftStream(WaveFormat WaveFormat, OpusStream OpusStream)
+        {
+            this.WaveFormat = WaveFormat;
+            DecodedAudio = new BufferedWaveProvider(WaveFormat) { ReadFully = true, BufferDuration = TimeSpan.FromSeconds(2) };
+            this.OpusStream = OpusStream;
+            TokenSource = new CancellationTokenSource();
+            Token = TokenSource.Token;
+
+            DecodeThread = Run();
+        }
+
+        public int Read(byte[] buffer, int offset, int count)
+        {
+            return DecodedAudio.Read(buffer, offset, count);
+        }
+
+        private Task Run()
+        {
+            return Task.Run(() => {
+                while (!Token.IsCancellationRequested)
+                {
+                    var buffer = new byte[WaveFormat.ConvertLatencyToByteSize(40)];
+                    var count = OpusStream.Read(buffer, 0, buffer.Length);
+                    DecodedAudio.AddSamples(buffer, 0, count);
+                }
+            }, Token);
+        }
+
+        public void Dispose()
+        {
+            TokenSource.Cancel();
+            TokenSource.Dispose();
+            DecodeThread.Dispose();
+        }
+    }
+}
