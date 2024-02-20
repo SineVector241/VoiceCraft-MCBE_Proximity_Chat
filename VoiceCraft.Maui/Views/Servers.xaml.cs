0780a16698a9f382af9cad332dbeaef544a28f46
SineVector241 <flyingbirdasuka1@gmail.com>
2024-02-20 23:02:42 +1100

    I was expecting it to crash, but it didn't...

--- VoiceCraft.Maui/Views/Servers.xaml.cs
+++ VoiceCraft.Maui/Views/Servers.xaml.cs
@@ -1,18 +1,20 @@
-﻿namespace VoiceCraft.Maui
+﻿using OpusSharp;
+
+namespace VoiceCraft.Maui.Views
 {
-    public partial class MainPage : ContentPage
+    public partial class Servers : ContentPage
     {
         int count = 0;
 
-        public MainPage()
+        public Servers()
         {
             InitializeComponent();
+            var enc = new OpusDecoder(48000, 1);
         }
 
         private void OnCounterClicked(object sender, EventArgs e)
         {
             count++;
-
             if (count == 1)
                 CounterBtn.Text = $"Clicked {count} time";
             else
