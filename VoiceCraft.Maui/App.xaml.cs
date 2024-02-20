9f4455514356772da1af9091699e1200d993b97a
SineVector241 <flyingbirdasuka1@gmail.com>
2024-02-20 20:42:25 +1100

    Maui Project

---
+++ VoiceCraft.Maui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace VoiceCraft.Maui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
