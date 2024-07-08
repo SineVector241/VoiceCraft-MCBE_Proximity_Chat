649b46f57c1d0317d9fb3f2a4f8acbafd3ea09d0
SineVector241 <69619913+SineVector241@users.noreply.github.com>
2024-07-08 23:17:45 +1000

    Reminder

--- VoiceCraft.Core/Packets/MCComm/GetChannels.cs
+++ VoiceCraft.Core/Packets/MCComm/GetChannels.cs
@@ -5,6 +5,6 @@ namespace VoiceCraft.Core.Packets.MCComm
     public class GetChannels : MCCommPacket
     {
         public override byte PacketId => (byte)MCCommPacketTypes.GetChannels;
-        public List<Channel> Channels { get; set; } = new List<Channel>();
-}
+        public List<Channel> Channels { get; set; } = new List<Channel>(); //FIX THIS
+    }
 }
