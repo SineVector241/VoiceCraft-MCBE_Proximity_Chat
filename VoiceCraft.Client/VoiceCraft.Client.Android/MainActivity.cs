c4b5d94fd2dd780dd143a8f598a9c63111d3a00d
SineVector241 <69619913+SineVector241@users.noreply.github.com>
2024-09-17 16:26:45 +1000

    I'm just trying anything at this point to get OpenAL to work...

--- VoiceCraft.Client/VoiceCraft.Client.Android/MainActivity.cs
+++ VoiceCraft.Client/VoiceCraft.Client.Android/MainActivity.cs
@@ -16,6 +16,7 @@ namespace VoiceCraft.Client.Android
     {
         protected override AppBuilder CustomizeAppBuilder(AppBuilder builder)
         {
+            Java.Lang.JavaSystem.LoadLibrary("openal");
             return base.CustomizeAppBuilder(builder)
                 .WithInterFont();
         }
---
+++ VoiceCraft.Client/VoiceCraft.Client/Audio/AudioSource.cs
@@ -0,0 +1,34 @@
+﻿using Silk.NET.OpenAL;
+
+namespace VoiceCraft.Client.Audio
+{
+    public unsafe class AudioSource : Core.Audio.AudioSource
+    {
+        public readonly Device* Device;
+
+        private readonly ALContext _alContext;
+        private readonly AL _al;
+        private readonly Context* _context;
+
+        public AudioSource(string device)
+        {
+            _alContext = ALContext.GetApi();
+            _al = AL.GetApi();
+
+            Device = _alContext.OpenDevice(device);
+            if (Device == null)
+            {
+                throw new AudioDeviceException("Could not create device!");
+            }
+
+            _context = _alContext.CreateContext(Device, null);
+            if (_context == null)
+            {
+                throw new AudioDeviceException($"Could not create device context! Error: {_al.GetError()}");
+            }
+            _alContext.MakeContextCurrent(_context);
+
+
+        }
+    }
+}
