da22aa677bb022ec08531d5538260fa634c751c8
SineVector241 <69619913+SineVector241@users.noreply.github.com>
2024-09-28 11:09:01 +1000

    Add more to navigation service.

--- VoiceCraft.Client.PDK/Services/NavigationService.cs
+++ VoiceCraft.Client.PDK/Services/NavigationService.cs
@@ -5,24 +5,45 @@ namespace VoiceCraft.Client.PDK.Services
     public class NavigationService
     {
         public Control CurrentPage { get; private set; } = default!;
+        public int MaxHistory { get; }
 
         public event EventHandler<Control>? OnPageChanging;
         public event EventHandler<Control>? OnPageChanged;
 
         private Func<Type, Control> _createView;
+        private List<Control> _history;
 
-        public NavigationService(Func<Type, Control> createView)
+        public NavigationService(Func<Type, Control> createView, int maxHistory = 50)
         {
             _createView = createView;
+            _history = new List<Control>();
+            MaxHistory = maxHistory;
         }
 
         public TPage NavigateTo<TPage>() where TPage : Control
         {
             var page = InstantiatePage<TPage>();
             OnPageChanging?.Invoke(this, page);
+
             CurrentPage = page;
+            if (_history.Count >= MaxHistory)
+                _history.RemoveAt(0);
+            _history.Add(page);
+
             OnPageChanged?.Invoke(this, CurrentPage);
             return page;
+        }
+
+        public void Back()
+        {
+            if(_history.Count > 1)
+            {
+                _history.RemoveAt(_history.Count - 1);
+                var page = _history.Last();
+                OnPageChanging?.Invoke(this, page);
+                CurrentPage = page;
+                OnPageChanged?.Invoke(this, CurrentPage);
+            }
         }
 
         private T InstantiatePage<T>()
