60831a970a4b01418311f7ef711d42d8cb681a5a
SineVector241 <69619913+SineVector241@users.noreply.github.com>
2024-10-12 08:43:47 +1100

    Add macOS platform specific project.

---
+++ VoiceCraft.Client.MacOS/Program.cs
@@ -0,0 +1,33 @@
+﻿using Avalonia;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using VoiceCraft.Client.PDK.Audio;
+
+namespace VoiceCraft.Client.MacOS
+{
+    internal sealed class Program
+    {
+        // Initialization code. Don't use any Avalonia, third-party APIs or any
+        // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+        // yet and stuff might break.
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            var avaloniaApp = BuildAvaloniaApp();
+
+            /*
+            App.Services.AddSingleton<IAudioPlayer, AudioPlayer>();
+            App.Services.AddSingleton<IAudioRecorder, AudioRecorder>();
+            App.Services.AddSingleton<IAudioDevices, AudioDevices>();
+            */
+            avaloniaApp.StartWithClassicDesktopLifetime(args);
+        }
+
+        // Avalonia configuration, don't remove; also used by visual designer.
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .WithInterFont()
+                .LogToTrace();
+    }
+}
